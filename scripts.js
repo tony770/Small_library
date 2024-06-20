d4174b91329ac3509a623387f0b6acb94cdf18d2
Tony <tonyzoudev@gmail.com>
2024-06-20 19:28:33 -0400

    basic javascript implemented

--- scripts.js
+++ scripts.js
@@ -1,13 +1,56 @@
 const myLibrary = [];
 
-function Book(author, title, pageNum, read) {
-    this.author = author;
+function Book(title, author, pageNum, read) {
     this.title = title;
+    this.author = author;
     this.pageNum = pageNum;
     this.read = read;
 }
 
-function addBookToLibrary(author, title, pageNum, read) {
-    const newBook = new Book(author, title, pageNum, read);
-    myLibrary.push(newBook);
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('#bookForm');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const title = document.querySelector('#bookTitle').value;
+        const author = document.querySelector('#authorName').value;
+        const pageNum = document.querySelector('#pageNum').value;
+        const read = document.querySelector('#read').checked;
+
+        const newBook = new Book(title, author, pageNum, read);
+        myLibrary.push(newBook);
+        displayBook();
+        form.reset();
+    })
+})
+
+function displayBook() {
+    const card_container = document.querySelector('.card_container');
+    card_container.innerHTML = '';
+
+    myLibrary.forEach((book) => {
+        const newDiv = document.createElement('div');
+        newDiv.className = 'card';
+        
+        const newAuthor = document.createElement('h2');
+        newAuthor.innerText = `${book.author}`;
+        newDiv.appendChild(newAuthor);
+
+        const newTitle = document.createElement('p');
+        newTitle.innerText = `${book.title}`;
+        newDiv.appendChild(newTitle);
+
+        const newPageNum = document.createElement('p');
+        newPageNum.innerText = `${book.pageNum}`;
+        newDiv.appendChild(newPageNum);
+
+        const newRead = document.createElement('p');
+        newRead.innerText = book.read ? 'Status: Read' : 'Status : Not Read';
+        newDiv.appendChild(newRead);
+
+        card_container.appendChild(newDiv);
+    });
+
 }
+
